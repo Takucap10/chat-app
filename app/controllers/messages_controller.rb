bc13a2fa71142db8cf4bdc9f9a0f41b0ec17900a
Takucap10 <t.okusako10@gmail.com>
2021-10-16 12:30:26 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,23 @@
 class MessagesController < ApplicationController
  
   def index
+    @message = Message.new
+    @room = Room.find(params[:room_id])
   end
+
+  def create
+    @room = Room.find(params[:room_id])
+    @message = @room.messages.new(message_params)
+    if @message.save
+      redirect_to room_message_path(@room)
+    end 
+      render :index
+  end
+
+ private
+  def message_params
+    params.require(:message).permit(:content).merge(use_id: current_user.id)
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root to: "rooms#index"
   devise_for :users
-  resources :messages, only:[:get]
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new, :create]
+  resources :rooms, only: [:new, :create] do
+    resources :messages, only:[:index, :create]
+  end
 end
